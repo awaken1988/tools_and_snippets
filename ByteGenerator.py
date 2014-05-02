a9db3a0008e0f1f7e37de8d23ee86a39f2948139
martin <martin.awake1@googlemail.com>
2014-05-02 11:39:45 +0200

    basic functions

--- ByteGenerator.py
+++ ByteGenerator.py
@@ -3,4 +3,33 @@ Created on 2 May 2014
 
 @author: martin
 '''
+import random
 
+
+def generateRandom(aStream, aSizeByte):
+    random.seed()    
+    for i in range(0, aSizeByte):
+        currVal = bytes([random.randrange(0, 256)])
+        aStream.write(currVal)
+        
+def generateCounter(aStream, aSizeByte, aStart, aIncLen):
+    random.seed()
+    
+    currIncLen = 0
+    for i in range(0, aSizeByte):
+        if aStart > 255: aStart = 0;
+        currVal = bytes([aStart])
+        aStream.write(currVal)
+        currIncLen = currIncLen + 1
+        if currIncLen >= aIncLen:
+            currIncLen = 0
+            aStart     = aStart + 1;
+
+
+            
+            
+        
+
+myFile = open("test.bin", "wb")        
+
+generateCounter(myFile, 256, 3, 4)
