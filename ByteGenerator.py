9bb72c46a781e7f4a186487f24ca5b8d464ca09c
martin <martin.awake1@googlemail.com>
2014-05-03 18:38:19 +0200

    the gui is nearly ready

--- ByteGenerator.py
+++ ByteGenerator.py
@@ -3,9 +3,10 @@ Created on 2 May 2014
 
 @author: martin
 '''
+from __future__ import generators
 import random
 from tkinter import Tk, Canvas, Frame, BOTH, LEFT, Text, Label, Button, Y, X, BOTTOM,\
-    Spinbox
+    Spinbox, SUNKEN, NW
 from tkinter import StringVar, OptionMenu
 
 
@@ -47,21 +48,21 @@ class GuiBasicSettings(Frame):
     
     def _initFile(self):
         self._fileFrm = Frame(self)
-        self._fileFrm.pack()
-        self._fileLbl = Label(self._fileFrm, text="File: ")
+        self._fileFrm.pack(padx=10, anchor=NW)
+        self._fileLbl = Label(self._fileFrm, text="File:       ")
         self._fileLbl.pack(side=LEFT)
         self._fileTxt = Text(self._fileFrm, height=1, width=30)
-        self._fileTxt.pack(side=LEFT)
+        self._fileTxt.pack(side=LEFT, fill=X, expand=1)
         self._fileBtn = Button(self._fileFrm, text="Create", command=self._callbackFun)
         self._fileBtn.pack(side=LEFT)
     
     def _initSize(self):
         self._sizeFrm = Frame(self)
-        self._sizeFrm.pack(fill=X, expand=1)
-        self._sizeLbl = Label(self._sizeFrm, text="FileSize")
+        self._sizeFrm.pack(padx=10, anchor=NW)
+        self._sizeLbl = Label(self._sizeFrm, text="FileSize:  ")
         self._sizeLbl.pack(side=LEFT)
         self._sizeTxt = Text(self._sizeFrm, height=1, width=20)
-        self._sizeTxt.pack(side=LEFT)
+        self._sizeTxt.pack(side=LEFT, fill=X, expand=1)
         
         self._sizeVar    = StringVar()
         self._sizeVar.set("Byte")       #FIXME: replace "Byte" with variable
@@ -97,6 +98,11 @@ class GuiGenerateRandom(Frame):
         
     def getSettings(self):
         return {}
+    
+    def getName(self):
+        return "Random"
+
+
 
 class GuiGenerateCount(Frame):
     def __init__(self, parent):
@@ -118,16 +124,42 @@ class GuiGenerateCount(Frame):
         return  {   "StepLen":      self._stepLenSpin.get(),
                     "StartValue":   self._startTxt.get()
                 }
-        
-        
-        
     
+    def getName(self):
+        return "Counter"
         
+        
+        
+class GuiGeneratorSelect(Frame):
+    def __init__(self, parent, generators):
+        Frame.__init__(self, parent)
+        self.parent = parent
+        self.pack()
+        
+        self._generators = generators
+        self._generatorName = StringVar()
+        self._generatorName.set(generators[0])
+        self._generatorName.trace("w", self._switchSettings)
+        
+        self._generatorLbl = Label(self, text="Generator");
+        self._generatorLbl.pack(side=LEFT)
+        
+        param = (self, self._generatorName) + tuple(i.getName() for i in generators)
+        self._generatorOpt = OptionMenu(*param)
+        self._generatorOpt.pack(side=LEFT)
+        
+    def _switchSettings(self, *args):
+       print("DBG: switch generator settings")
+       for i in self._generators:
+           if i.getName() == self._generatorName.get(): 
+               i.pack()
+               print("pack " + str(i.getName()))
+           else:
+               i.pack_forget() 
+               print("unpack " + str(i.getName()))
+                
 
-    
-        
-        
-    
+
 
 class MainWindow(Frame):        
     def __init__(self, parent):
@@ -135,14 +167,39 @@ class MainWindow(Frame):
         self.parent = parent
         self.pack(fill=BOTH, expand=1)
         
+        
         #basic settings
         self.basicSettings = GuiBasicSettings(self); self.basicSettings.pack()
         
+        #separatr
+        self._generatorSepFrm = Frame(self, bg="#000000", height=2)
+        self._generatorSepFrm.pack(fill=X, expand=1, pady=20)
+        
+        # generator description
+        genDescStr =                "Here you choose how the Binary file is generated\n"
+        genDescStr = genDescStr +   " * Random:  creates an random image\n"
+        genDescStr = genDescStr +   " * Counter: create a pattern like 112233 or 111122223333"
+        
+        self._generatorDescLbl = Label(self, text=genDescStr, justify=LEFT)
+        self._generatorDescLbl.pack(ipadx=10)
+        
+        #generator select frames
+        self._generatorSelectFrm = Frame(self)
+        self._generatorSelectFrm.pack(pady=10)
+        
         #generators
-        self.generatorFrames = ( GuiGenerateRandom(self), GuiGenerateCount(self) )
+        self._generatorFrm = Frame(self, relief=SUNKEN, bd=2)
+        self._generatorFrm.pack(fill=X, expand=1, padx=20)
+        self.generatorFrames = ( GuiGenerateRandom(self._generatorFrm), 
+                                 GuiGenerateCount(self._generatorFrm) 
+                                )
         for i in self.generatorFrames:
             i.pack_forget()
+            
+        #generator select frames (self.generatorFrames)
+        self_generatorSelect = GuiGeneratorSelect(self._generatorSelectFrm, self.generatorFrames)
     
+        
         
         
         
