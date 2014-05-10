95a2202e12bd22aaa428895595e5b43b806cf344
martin <martin.awake1@googlemail.com>
2014-05-10 11:51:41 +0200

    working on the gui

--- ByteGenerator.py
+++ ByteGenerator.py
@@ -3,27 +3,34 @@ Created on 2 May 2014
 
 @author: martin
 '''
-from __future__ import generators
 import random
 from tkinter import Tk, Canvas, Frame, BOTH, LEFT, Text, Label, Button, Y, X, BOTTOM,\
-    Spinbox, SUNKEN, NW
+    Spinbox, SUNKEN, NW, Entry
 from tkinter import StringVar, OptionMenu
 
 
-def generateRandom(aStream, aSizeByte):
+
+def generateRandom(aFileSize, aSpecificSettings): 
     random.seed()    
-    for i in range(0, aSizeByte):
+    for i in range(0, aFileSize):
         currVal = bytes([random.randrange(0, 256)])
-        aStream.write(currVal)
+        yield currVal
         
-def generateCounter(aStream, aSizeByte, aStart, aIncLen):
-    random.seed()
+def generateCounter(aFileSize, aSpecificSettings):
+    aStart  = 0
+    aIncLen = 0
+    
+    try:    aStart = int(aSpecificSettings["StartValue"])
+    except: raise Exception("StartValue is invalid")
+    
+    try:    aIncLen = int(aSpecificSettings["StepLen"])
+    except: raise Exception("IncLen is invalid")
     
     currIncLen = 0
-    for i in range(0, aSizeByte):
+    for i in range(0, aFileSize):
         if aStart > 255: aStart = 0;
         currVal = bytes([aStart])
-        aStream.write(currVal)
+        yield currVal
         currIncLen = currIncLen + 1
         if currIncLen >= aIncLen:
             currIncLen = 0
@@ -51,7 +58,8 @@ class GuiBasicSettings(Frame):
         self._fileFrm.pack(padx=10, anchor=NW)
         self._fileLbl = Label(self._fileFrm, text="File:       ")
         self._fileLbl.pack(side=LEFT)
-        self._fileTxt = Text(self._fileFrm, height=1, width=30)
+        self._fileTxt = Entry(self._fileFrm)
+        self._fileTxt.insert(0, "/tmp/out.txt")
         self._fileTxt.pack(side=LEFT, fill=X, expand=1)
         self._fileBtn = Button(self._fileFrm, text="Create", command=self._callbackFun)
         self._fileBtn.pack(side=LEFT)
@@ -61,7 +69,8 @@ class GuiBasicSettings(Frame):
         self._sizeFrm.pack(padx=10, anchor=NW)
         self._sizeLbl = Label(self._sizeFrm, text="FileSize:  ")
         self._sizeLbl.pack(side=LEFT)
-        self._sizeTxt = Text(self._sizeFrm, height=1, width=20)
+        self._sizeTxt = Entry(self._sizeFrm)
+        self._sizeTxt.insert(0, "1024")
         self._sizeTxt.pack(side=LEFT, fill=X, expand=1)
         
         self._sizeVar    = StringVar()
@@ -78,7 +87,7 @@ class GuiBasicSettings(Frame):
         return self._fileTxt.get()
         
     def getFileSize(self):
-        mult = self.sizeUnits[self._sizeVar]
+        mult = self.sizeUnits[self._sizeVar.get()]
         val  = self._sizeTxt.get()
         return val * mult   
     
@@ -101,6 +110,9 @@ class GuiGenerateRandom(Frame):
     
     def getName(self):
         return "Random"
+    
+    def getGeneratorFunction(self):
+        return generateRandom;
 
 
 
@@ -116,9 +128,10 @@ class GuiGenerateCount(Frame):
         self._stepLenSpin = Spinbox(self._stepLenFrm, from_=0, to=1000); self._stepLenSpin.pack(side=LEFT)
         
         #start value
-        self._startFrm  = Frame(self)
+        self._startFrm  = Frame(self); self._startFrm.pack()
         self._startLbl  = Label(self._startFrm, text="Start Value: ");   self._startLbl.pack(side=LEFT)
-        self._startTxt  = Text(self._startFrm, width=20, height=1);   self._startTxt.pack(side=LEFT)
+        self._startTxt  = Entry(self._startFrm);   self._startTxt.pack(side=LEFT)
+        self._startTxt.insert(0, "0")
         
     def getSettings(self):
         return  {   "StepLen":      self._stepLenSpin.get(),
@@ -128,6 +141,8 @@ class GuiGenerateCount(Frame):
     def getName(self):
         return "Counter"
         
+    def getGeneratorFunction(self):
+        return generateCounter
         
         
 class GuiGeneratorSelect(Frame):
@@ -138,7 +153,7 @@ class GuiGeneratorSelect(Frame):
         
         self._generators = generators
         self._generatorName = StringVar()
-        self._generatorName.set(generators[0])
+        self._generatorName.set(generators[0].getName())
         self._generatorName.trace("w", self._switchSettings)
         
         self._generatorLbl = Label(self, text="Generator");
@@ -148,15 +163,23 @@ class GuiGeneratorSelect(Frame):
         self._generatorOpt = OptionMenu(*param)
         self._generatorOpt.pack(side=LEFT)
         
+        
+        self._switchSettings()
+        
     def _switchSettings(self, *args):
        print("DBG: switch generator settings")
        for i in self._generators:
            if i.getName() == self._generatorName.get(): 
                i.pack()
+               self._generatorGui = i
                print("pack " + str(i.getName()))
            else:
                i.pack_forget() 
                print("unpack " + str(i.getName()))
+    
+    def getCurrGeneratorGui(self):
+        return self._generatorGui         
+    
                 
 
 
@@ -170,6 +193,7 @@ class MainWindow(Frame):
         
         #basic settings
         self.basicSettings = GuiBasicSettings(self); self.basicSettings.pack()
+        self.basicSettings.setCallback(self.generateFile)
         
         #separatr
         self._generatorSepFrm = Frame(self, bg="#000000", height=2)
@@ -197,8 +221,60 @@ class MainWindow(Frame):
             i.pack_forget()
             
         #generator select frames (self.generatorFrames)
-        self_generatorSelect = GuiGeneratorSelect(self._generatorSelectFrm, self.generatorFrames)
+        self._generatorSelect = GuiGeneratorSelect(self._generatorSelectFrm, self.generatorFrames)
     
+        #error output
+        self._infoFrm = Frame(self, relief=SUNKEN, bd=2)
+        self._infoFrm.pack(fill=X, expand=1, pady=10) 
+        self._infoLbl = Label(self, bg="#000000", text="nothing to do", fg="#00FF00")
+        self._infoLbl.pack(fill=X, expand=1, padx=0)     
+        self._infoColorNormal = "#00FF00"
+        self._infoColorErr    = "#FF0000"
+    
+    def generateFile(self):
+        print("generate")
+        
+        fileName        = self.basicSettings.getFileName()
+        fileSize        = 0
+        blockSize       = 512
+        currGeneratorGui    = self._generatorSelect.getCurrGeneratorGui()
+
+        print(type(currGeneratorGui))
+        currGenerator       = currGeneratorGui.getGeneratorFunction()
+        specSettings        = currGeneratorGui.getSettings()
+        
+        
+        #cast filesize
+        try:
+            fileSize = int(self.basicSettings.getFileSize())
+            if fileSize < 1:
+                raise Exception("")
+        except:
+            self._infoLbl.config(fg=self._infoColorErr, text="file size is invalid")
+            return
+        
+        #open file
+        try:
+            currFile = open(fileName, "wb")
+        except:
+            self._infoLbl.config(fg=self._infoColorErr, text="cannot create file")
+            return
+        
+        #write to file
+        try:
+            buff = b""
+            for i in currGenerator(fileSize, specSettings):
+                buff += i
+                if len(buff) > blockSize:
+                    currFile.write(buff)
+                    buff = b""
+            if len(buff) > 0:
+                 currFile.write(buff)
+                 buff = None
+        except as exc:
+            print ex
+       
+        
         
         
         
@@ -206,11 +282,22 @@ class MainWindow(Frame):
 root    = Tk()
 ex      = MainWindow(root)
 root.mainloop()
-        
 
+a = """
+file = open("/tmp/test.bin", "wb")
 
+fileSize    = 2200
+blockSize   = 512
+buff        = b""
+for i in generateCounter(fileSize, 0, 8 ):
+    buff += i
+    if len(buff) > blockSize:
+        file.write(buff)
+        buff = b""
 
-
+if len(buff) > 0:
+    file.write(buff)
+"""
 
 
 
