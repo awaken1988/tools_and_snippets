b985d661798c3ee2d25dd2f31ee1ebe1c019e9be
Martin <martin.awake1@gmail.com>
2017-08-19 14:34:02 +0200

    add new pointer

--- dirDiff.rs
+++ dirDiff.rs
@@ -4,11 +4,12 @@ use std::path::Path;
 use std::collections::HashMap;
 use std::fmt;
 use std::rc::Rc;
+use std::cell::RefCell;
 
 #[derive(Debug)]
 struct PathTree {
-    children: HashMap<String, Rc<PathTree>>,
-    parent: Option<Rc<PathTree>>,
+    children: HashMap<String, Rc<RefCell<PathTree>>>,
+    parent: Option<Rc<RefCell<PathTree>>>,
     name : String,
     is_dir : bool,
 }
@@ -24,7 +25,7 @@ impl PathTree {
 
         if self.is_dir {
             for child in self.children.values() {
-                child.walkprint( depth+1, f );
+                child.borrow().walkprint( depth+1, f );
             }
         }
     }
@@ -39,37 +40,54 @@ impl fmt::Display for PathTree {
     }
 }
 
+//impl PathTree {
+    fn full_path(ptree: &PathTree) -> String
+    {
+        let mut ret = String::new();
+
+        let parent = (ptree.parent.as_ref()).unwrap();
+        let inner_str = full_path(&*parent.borrow());
+        //let ddd: () = &*ptree.parent.unwrap().borrow();
+
+        ret = inner_str + "/" + &ret;
+
+        return ret;
+    }
+//}
 
 
-fn walkdir(dir: &Path) -> Rc<PathTree>
+
+fn walkdir(dir: &Path) -> Rc<RefCell<PathTree>>
 {
-    let mut ret = PathTree{ name: dir.file_name().unwrap().to_str().unwrap().to_string(),
+    let mut ret = Rc::new(RefCell::new(PathTree{ name: dir.file_name().unwrap().to_str().unwrap().to_string(),
                             parent: None,
                             is_dir: false,
-                            children: HashMap::new()};
+                            children: HashMap::new()}));
     if dir.is_file()  {
         //println!("{}", dir.to_str().unwrap());
     } else {
         let dir_entries = fs::read_dir(dir.to_str().unwrap()).unwrap();
-        ret.is_dir = true;
+        ret.borrow_mut().is_dir = true;
 
         for entry in dir_entries {
             let child = walkdir(& entry.unwrap().path());
 
-            ret.children.insert(child.name.clone(), child);
-            //child.parent = Some(Rc::new(ret))
+            let name = child.borrow().name.clone();
+
+            ret.borrow_mut().children.insert(name, child.clone());
+            child.borrow_mut().parent = Some(ret.clone());
         }
     }
-    return Rc::new(ret);
+    return ret;
 }
 
 fn compare_dir(left: &PathTree, right: &PathTree)
 {
     for (iPath, iChild) in left.children.iter() {
-        if( !right.children.contains_key(iPath) ) {
-            println!("-{}", iPath)
+        if !right.children.contains_key(iPath)  {
+            println!("-{}", full_path(&*iChild.borrow()))
         } else {
-            compare_dir(&*left.children[iPath],&*right.children[iPath]);
+            compare_dir(&*left.children[iPath].borrow(), &*right.children[iPath].borrow());
         }
     }
 }
@@ -78,8 +96,13 @@ fn main()
 {
     let left = walkdir(Path::new("./left"));
     let right = walkdir(Path::new("./right"));
-    //walkprint(&path_tree, 0);
-    //println!("{}", path_tree);
 
-    compare_dir(&*left, &*right);
+    //example print dir
+    //println!("{}", *left.borrow());
+    //println!("{}", *right.borrow());
+
+    //example: print type of variable
+    //let ddd: () = *left.borrow();
+
+    compare_dir(&*left.borrow(), &*right.borrow());
 }
