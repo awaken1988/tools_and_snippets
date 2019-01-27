8bd15259546dae13bb2c3b6a944d0da39c38ceed
Martin <martin.awake1@gmail.com>
2019-01-27 19:10:07 +0100

    recursively list directories

---
+++ rust_snippets/mult_dir_diff/src/diff_tool.rs
@@ -0,0 +1,137 @@
+use std::collections::HashMap;
+use std::collections::BTreeSet;
+use std::path::Path;
+use std::path::PathBuf;
+use std::fs::read_dir;
+use std::fs::DirEntry;
+use std::fs::metadata;
+   
+#[derive(Debug)]
+pub struct DiffItem {
+    pub parent: Option<usize>,
+    pub child: Vec<usize>,
+    pub path: Vec<Option<PathBuf>>,
+}
+
+#[derive(Debug)]
+pub struct DiffTree {
+    pub entries: Vec<DiffItem>,
+}
+
+pub fn expand_dirs( dirs: &Vec<Option<PathBuf>>, 
+                    tree: &mut DiffTree,
+                    parent: Option<usize>) 
+{
+    let real_parent = if let Some(x) = parent {
+        x
+    }
+    else { 
+        let mut root_item = DiffItem {  parent: parent, path: dirs.clone(), child: vec![], };
+        tree.entries.push(root_item);
+        tree.entries.len()-1 
+    };
+
+    //iterate over all DirEntries and add it to tree
+    let mut included = HashMap::new();  //each dir should only get one numer; 
+                                        //TODO: is HashMap and colission?
+    let mut sub_dirs = BTreeSet::new();
+
+    //list all files,dirs 
+    let start_idx = tree.entries.len();
+    for (i_diff_idx, i_dir) in dirs.iter().enumerate() {
+        if let Some(i_dir)  = i_dir {
+              list_dir(&i_dir, 
+                &mut |item,_isdir| { 
+                    let curr_path = item.path().to_path_buf();
+                    let curr_last_part = curr_path.file_name().unwrap().to_os_string();
+
+                    if !included.contains_key(&curr_last_part) {
+                        included.insert(curr_last_part.clone(), tree.entries.len());
+                        let mut diff_item = DiffItem {  parent: Some(real_parent), 
+                                                        path: vec![None; dirs.len()], 
+                                                        child: vec![], };
+                        tree.entries.push(diff_item);
+                        let curr_idx = tree.entries.len()-1;
+                        tree.entries[real_parent].child.push(curr_idx); 
+                    }
+                    
+                    let idx = match included.get(&curr_last_part) {
+                        Some(x) => x,
+                        None => panic!("diff_entry should already be included in the HashMap"),
+                    };
+
+                    tree.entries[*idx].path[i_diff_idx] = Some(curr_path.clone()); 
+
+                    if curr_path.metadata().unwrap().is_dir() {
+                        sub_dirs.insert(*idx);
+                    }
+                });
+        }
+    }
+    let end_idx = tree.entries.len();
+
+    //add all subdirs
+    for i_dir in sub_dirs {
+        expand_dirs( &tree.entries[i_dir].path.clone(), tree, Some(i_dir) );
+    } 
+}
+
+pub fn print_tree_flat(tree: &DiffTree) {
+    for (i_idx, i) in tree.entries.iter().enumerate() {
+        println!("{} {:?}",i_idx, i);
+    }
+}
+
+pub fn print_tree(tree: &DiffTree, parent: usize, depth: usize) {
+
+    let mut dept_str = String::new();
+    for i in 0..(depth*4) {
+        dept_str += " ";
+    }
+
+    for i_child in &tree.entries[parent].child {
+        let curr_entry = &tree.entries[*i_child].path;
+        
+        println!("{}#------------------------------------", dept_str);
+        for (j_num, j_path) in curr_entry.iter().enumerate() {
+            if let Some(j_path) = j_path {
+                println!("{}| {}: Result={:?}", dept_str, j_num, j_path.file_name());
+            } 
+            else {
+                println!("{}| {}: ", dept_str, j_num);
+            }
+        }
+        println!("");
+
+        print_tree(tree, *i_child, depth+1);
+
+    }
+}
+
+fn list_dir(dir: &Path, 
+            fun: &mut dyn FnMut(DirEntry, bool) ) 
+{
+    let dir_iter = read_dir(dir);
+
+    if let Ok(dir_iter) = dir_iter {
+        for i_entry in dir_iter {
+            let i_entry = i_entry.unwrap();
+            let i_meta  = i_entry.metadata().unwrap();
+            
+            if i_meta.is_dir() {
+                fun(i_entry, true);
+            }
+            else if i_meta.is_file() {
+                fun(i_entry, false);
+            }
+            else {
+                println!("[WRN] ignored: {:?}", i_entry);
+            }
+        }
+    }
+    else if let Err(e) = dir_iter {
+        println!("list_dir: {:?}", e);
+    }
+
+
+}
--- rust_snippets/mult_dir_diff/src/main.rs
+++ rust_snippets/mult_dir_diff/src/main.rs
@@ -1,102 +1,22 @@
-use std::collections::HashMap;
-use std::path::Path;
-use std::fs::read_dir;
-use std::fs::DirEntry;
+use std::path::PathBuf;
+
+mod diff_tool;
 
 //TODO: what exactly is &mut dyn FnMut :-O ???
 //TODO: why  info: Vec<Option<Box<DirEntry>>> doesn't work
 
-struct DiffItem {
-    parent: Option<usize>,
-    info: Vec<Option<usize>>,
-    child: Vec<usize>,
-}
-
-struct DiffTree {
-    entries: Vec<DiffItem>,
-    info: Vec<DirEntry>,
-}
-
-
-
-fn list_dir(dir: &Path, 
-            fun: &mut dyn FnMut(DirEntry, bool) ) 
-{
-    for iEntry in read_dir(dir).unwrap() {
-        let iEntry = iEntry.unwrap();
-        let iMeta  = iEntry.metadata().unwrap();
-        
-        if iMeta.is_dir() {
-            fun(iEntry, true);
-        }
-        else if iMeta.is_file() {
-            fun(iEntry, false);
-        }
-        else {
-            println!("[WRN] ignored: {:?}", iEntry);
-        }
-    }
-}
-
-fn compare_dir( dirs: &[&Path], 
-                tree: &mut DiffTree,
-                parent: Option<usize>) 
-{
-    let mut included = HashMap::new();
-    let start_idx = tree.entries.len();
-    for (iDiffIdx, iDir) in dirs.iter().enumerate() {
-        list_dir(&iDir, 
-        &mut |item,_isdir| { 
-            tree.info.push( item );
-            let info_idx = tree.info.len()-1;
-            let info = &tree.info[info_idx];
-            let path_last_part = info.path().file_name().unwrap().to_os_string();
-
-            if !included.contains_key(&path_last_part) {
-                included.insert(path_last_part.clone(), tree.entries.len());
-                let mut diff_item = DiffItem {  parent: None, 
-                                                info: vec![None; dirs.len()], 
-                                                child: vec![], };
-                tree.entries.push(diff_item);
-            }
-            
-            let idx = match included.get(&path_last_part) {
-                Some(x) => x,
-                None => panic!("diff_entry should already be included in the HashMap"),
-            };
-
-            tree.entries[*idx].info[iDiffIdx] = Some(info_idx); 
-        });
-    }
-    let end_idx = tree.entries.len();
-}
-
-fn print_tree(tree: &DiffTree) {
-    for i in &tree.entries {
-        if let Some(x) = i.parent {
-            break;
-        }
-
-        println!("#------------------------------------");
-        for (jNum, jIdx) in i.info.iter().enumerate() {
-            if let Some(jIdx) = jIdx {
-                println!("| {}: Result={:?}", jNum, tree.info[*jIdx].path());
-            } 
-            else {
-                 println!("| {}: ", jNum);
-            }
-        }
-        println!("#------------------------------------");
-
-        println!("");
-    }
-}
-
 fn main() {
     println!("Hello, world!");
 
-    let dirs = [Path::new("testdata/left/"), Path::new("testdata/right/")];
-    let mut tree = DiffTree{ entries: vec![], info: vec![] };
-    compare_dir( &dirs, &mut tree, None );
-    print_tree( &tree );
+    let dirs = vec![
+        Some(PathBuf::from("testdata/1_left/")),
+        Some(PathBuf::from("testdata/2_right/")),
+        Some(PathBuf::from("testdata/3/")),
+    ];
+
+    let mut tree = diff_tool::DiffTree{ entries: vec![]};
+
+    diff_tool::expand_dirs( &dirs, &mut tree, None );
+    diff_tool::print_tree( &tree, 0, 0 );
+    diff_tool::print_tree_flat( &tree);
 }
