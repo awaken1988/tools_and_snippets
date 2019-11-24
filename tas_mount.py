cfc231804a5bb76506ceed94f341f6f6f8592f6e
Martin K <martin.awake1@gmail.com>
2019-11-24 19:40:03 +0100

    tas rework

--- tas_mount.py
+++ tas_mount.py
@@ -1,10 +1,5 @@
-import tkinter as tk
-import json
-import platform
-import subprocess
-import sys
-import os
-
+#Reference:
+#   - https://ss64.com/nt/
 
 #TODO: umount before mount
 #TODO: create symblink on Windows to Desktop
@@ -12,9 +7,19 @@ import os
 #TODO: windows ~/Desktop/<name>
 #TODO: make other share types avail
 
-if platform.system() == "Linux":    
+import tkinter as tk
+import json
+import platform
+import subprocess
+import sys
+import os
+
+IS_LINUX = platform.system() == "Linux"
+IS_WINDOWS = platform.system() == "Windows"
+
+if IS_LINUX:    
     pass
-elif platform.system() == "Windows":
+elif IS_WINDOWS:
     def execute_terminal(aCmd, aForeground):
         cmd = ["start"]
         if aForeground:
@@ -23,7 +28,26 @@ elif platform.system() == "Windows":
         print("execute_terminal: "+str(cmd))
         os.system(" ".join(cmd))
 
-    def mount_share(aData):
+class ServiceMount:
+    @staticmethod
+    def getActions(aEntry):
+        ret = {"labels": ("share")}
+
+        if IS_WINDOWS:
+            return (
+                {"name": "mount",   "action": ServiceMount.win_mount_share},
+                {"name": "open",    "action": ServiceMount.win_open_share},
+                {"name": "umount",  "action": ServiceMount.win_unmount_share},
+            )
+        return None
+
+    @staticmethod
+    def getDisplay(aEntry):
+        return ("{} {}").format(aEntry["name"], aEntry["share"])
+
+
+    @staticmethod
+    def win_mount_share(aData):
         fixed_path = aData["share"].replace('/', "\\")
         cmd = ["net", "use", fixed_path]
         if "user" in aData:
@@ -31,17 +55,16 @@ elif platform.system() == "Windows":
         execute_terminal(cmd, True)
         execute_terminal(["explorer.exe", fixed_path], False)
 
-    def open_share(aData):
+    @staticmethod
+    def win_open_share(aData):
         fixed_path = aData["share"].replace('/', "\\")
         execute_terminal(["explorer.exe", fixed_path], False)
 
-    def unmount_share(aData):
+    @staticmethod
+    def win_unmount_share(aData):
         fixed_path = aData["share"].replace('/', "\\")
         cmd = ["net", "use", fixed_path, "/delete"]
         execute_terminal(cmd, False)
-        
-else:
-    raise Exception("Your Platform not yet supported")
 
 def loadcfg(aCfg):
     f = open(aCfg, "r");
@@ -57,7 +80,13 @@ def useraction(aType, aData):
         open_share(aData)
     if "unmount" == aType:
         unmount_share(aData)
-        
+
+services = {
+    "smb": ServiceMount,
+}
+
+
+
 window = tk.Tk()
 window.title("mount drives")
 
@@ -65,19 +94,29 @@ window.title("mount drives")
 data = loadcfg("mount.json")
 print(data)
 
-iRow = 0
-for iMount in data["mount"]:
-    outer_lambda = lambda xType, xData: lambda: useraction(xType, xData)    
+iRow = 1
 
-    share_lbl = tk.Label(window, text=iMount["name"])
-    mnt_btn = tk.Button(window, text="mount", command=outer_lambda("mount", iMount) )
-    open_btn = tk.Button(window, text="open", command=outer_lambda("open", iMount) )
-    unmount_btn = tk.Button(window, text="unmount", command=outer_lambda("unmount", iMount) )
+for iEntry in data["mount"]:
+    iType = iEntry["type"]
+    if iType not in services:
+        print("WRN: ignore unkown entry {}".format(str(iEntry)))
+        continue
     
-    share_lbl.grid(row=iRow, column=3)
-    unmount_btn.grid(row=iRow, column=2)
-    mnt_btn.grid(row=iRow, column=1)
-    open_btn.grid(row=iRow, column=0)
+    iService = services[iType]
+
+    iCol = 0
+
+    #text
+    tk.Label(window, text=iService.getDisplay(iEntry)).grid(row=iRow, column=iCol)
+    iCol += 1
+
+    #buttons
+    for iAction in iService.getActions(iEntry):
+        outer_lambda = lambda aAction,aEntry: lambda: aAction["action"](aEntry)    
+        btn = tk.Button(window, text=iAction["name"], command=outer_lambda(iAction, iEntry))
+
+        btn.grid(row=iRow, column=iCol)
+        iCol += 1
 
     iRow = iRow + 1
 
