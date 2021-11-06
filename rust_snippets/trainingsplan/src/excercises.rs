866d5f1767520d5274adc4b5f4190663e50e743d
Martin K <martin.awake1@gmail.com>
2021-11-07 00:42:01 +0100

    excercise jquery works

--- rust_snippets/trainingsplan/src/excercises.rs
+++ rust_snippets/trainingsplan/src/excercises.rs
@@ -1,56 +1,72 @@
 use serde::{Deserialize, Serialize};
 use serde_json::Result;
 use std::collections::HashMap;
+use std::collections::HashSet;
 
-#[derive(Serialize)]
+#[derive(PartialEq, Eq, Hash, Clone, Serialize)]
 pub enum ExcerciseValueType {
-    Time=0, 
-    RepWeight=1,
+    Duration=0, 
+    Weigth=1,
+    Repetition=2,
 }
 
 #[derive(Serialize)]
 pub struct Excercise {
-    pub value_type: ExcerciseValueType,
+    pub value_type: HashSet<ExcerciseValueType>,
     pub name: &'static str,
 
 }
 
 impl Excercise {
-    pub fn new(name: &'static str, value_type: ExcerciseValueType) -> Excercise {
-        Excercise{name: name, value_type: value_type}
+    pub fn new(name: &'static str, value_type: &HashSet<ExcerciseValueType>) -> Excercise {
+        Excercise{name: name, value_type: value_type.clone()}
     }
 }
 
+macro_rules! insert_excercise_type {
+    ($typeset:expr, $type0:expr) => {
+        $typeset.insert($type0)
+    };
+    ($typeset:expr, $type0:expr, $($typex:expr),+) => {
+        $typeset.insert($type0);
+        insert_excercise_type!($typeset, $($typex),+);
+    };
+}
+
 macro_rules! insert_excercise {
-    ($m:expr, $l:expr) => {
-        if $m.contains_key($l.name) {
+    ($m:expr, $name:expr, $($types:expr),+) => {
+        if $m.contains_key($name) {
             panic!("Duplicates")
         }
 
-        $m.insert($l.name, $l)
+        let mut typeset = HashSet::new();
+
+        insert_excercise_type!(typeset, $($types),+);
+
+        $m.insert($name, Excercise::new($name, &typeset));
     };
 }
 
 pub fn get_excercices() -> HashMap<&'static str, Excercise> {
     let mut ret = HashMap::new();
 
-    insert_excercise!(ret, Excercise::new("Einbeinstand",             ExcerciseValueType::Time));          
-    insert_excercise!(ret, Excercise::new("Balance Kniebeuge",        ExcerciseValueType::Time));              
-    insert_excercise!(ret, Excercise::new("Pilates Schulter",         ExcerciseValueType::Time));              
-    insert_excercise!(ret, Excercise::new("Superman",                 ExcerciseValueType::Time));      
-    insert_excercise!(ret, Excercise::new("Blank mit Bewegung",       ExcerciseValueType::Time));              
-    insert_excercise!(ret, Excercise::new("Ausfallschritt",           ExcerciseValueType::Time));          
-    insert_excercise!(ret, Excercise::new("Handlauf",                 ExcerciseValueType::Time));      
-    insert_excercise!(ret, Excercise::new("Dehnen Bein Vorn/Hinten",  ExcerciseValueType::Time));                  
-    insert_excercise!(ret, Excercise::new("Dehnen Oberkörper",        ExcerciseValueType::Time));              
-    insert_excercise!(ret, Excercise::new("Kobra",                    ExcerciseValueType::Time));  
-    insert_excercise!(ret, Excercise::new("Hängen",                   ExcerciseValueType::Time));  
-    insert_excercise!(ret, Excercise::new("Stocktraining",            ExcerciseValueType::Time));          
-    insert_excercise!(ret, Excercise::new("Freies Traing",            ExcerciseValueType::Time));          
-    insert_excercise!(ret, Excercise::new("Terraband Rücken",         ExcerciseValueType::Time));              
-    insert_excercise!(ret, Excercise::new("Terraband Brust",          ExcerciseValueType::Time));          
-    insert_excercise!(ret, Excercise::new("Terraband Schulter",       ExcerciseValueType::Time));              
-    insert_excercise!(ret, Excercise::new("Terraband Bizeps/Trizeps", ExcerciseValueType::Time));                      
+    insert_excercise!(ret, "Einbeinstand",             ExcerciseValueType::Duration);          
+    insert_excercise!(ret, "Balance Kniebeuge",        ExcerciseValueType::Duration);              
+    insert_excercise!(ret, "Pilates Schulter",         ExcerciseValueType::Duration);              
+    insert_excercise!(ret, "Superman",                 ExcerciseValueType::Duration);      
+    insert_excercise!(ret, "Blank mit Bewegung",       ExcerciseValueType::Duration);              
+    insert_excercise!(ret, "Ausfallschritt",           ExcerciseValueType::Duration);          
+    insert_excercise!(ret, "Handlauf",                 ExcerciseValueType::Duration);      
+    insert_excercise!(ret, "Dehnen Bein Vorn/Hinten",  ExcerciseValueType::Duration);                  
+    insert_excercise!(ret, "Dehnen Oberkörper",        ExcerciseValueType::Duration);              
+    insert_excercise!(ret, "Kobra",                    ExcerciseValueType::Duration);  
+    insert_excercise!(ret, "Hängen",                   ExcerciseValueType::Duration);  
+    insert_excercise!(ret, "Stocktraining",            ExcerciseValueType::Duration);          
+    insert_excercise!(ret, "Freies Traing",            ExcerciseValueType::Duration);          
+    insert_excercise!(ret, "Terraband Rücken",         ExcerciseValueType::Duration);              
+    insert_excercise!(ret, "Terraband Brust",          ExcerciseValueType::Duration);          
+    insert_excercise!(ret, "Terraband Schulter",       ExcerciseValueType::Duration);              
+    insert_excercise!(ret, "Terraband Bizeps/Trizeps", ExcerciseValueType::Duration);                      
 
     ret
 }
