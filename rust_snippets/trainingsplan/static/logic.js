3a7df5045f618b85e443d6a7cc6f2ce99f057e54
Martin K <martin.awake1@gmail.com>
2021-12-19 01:34:03 +0100

    add excercise list li

---
+++ rust_snippets/trainingsplan/static/logic.js
@@ -0,0 +1,48 @@
+"use strict"
+
+function create_excercise_entry(aTemplate)
+{
+    let li      = $('<li></li>')
+    $(li).addClass("excercise_item");
+    
+    //excercise name
+    let excercise_name = $('<b></b>').text(aTemplate.name);
+    li.append(excercise_name);
+
+    aTemplate.data.value_type.forEach( function(aValueType) {
+        console.log(aValueType);
+        if(aValueType == 'Duration') {
+            let duration_name  = document.createTextNode('Duration: ');
+            let duration_input = $('<input type="number" />') ;
+            let duration_unit  = document.createTextNode('Seconds');
+
+            li.append(duration_name);
+            li.append(duration_input);
+            li.append(duration_unit);
+
+            $(duration_input).addClass("excercise_input_duration");
+        } 
+    });
+
+    //remove
+    let btn_remove = $('<input/>' , {
+        type: 'button',
+        value: 'Delete',
+        on: {
+            click: function(){
+                $(this).parent().remove();
+            }
+        }
+    });
+
+    li.append(btn_remove);
+
+    return li;
+}
+
+function sync_excercise()
+{
+    $('.excercise_item').each(function(){
+        console.log(this)
+    });
+}
