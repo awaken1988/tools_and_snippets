a82e3dd70a6d14502257f8381ca283b2b2220bbf
Martin K <martin.awake1@gmail.com>
2022-09-28 22:02:26 +0200

    WIP: adept btrfs backup helper script

--- tas_btrfs_localbackup.py
+++ tas_btrfs_localbackup.py
@@ -2,20 +2,53 @@
 
 import os
 import sys
+import argparse
 
-src  = sys.argv[1]
-dest = sys.argv[2]
+from os import path
 
-if src[-1]  == "/": src  = src[:-1]
-if dest[-1] == "/": dest = dest[:-1]
 
-src_name  = os.path.basename(src)
-src_snaps = src + "/../.snapshot_"+src_name
+def errorReturn(aMsg):
+    print(aMsg)
+    sys.exit(1)
+
+def scriptDir():
+    return os.path.dirname(__file__)
+
+def scriptRelative(aPath):
+    if os.path.isabs(aPath):    return aPath
+    else:                       return path.abspath(path.join(scriptDir(), aPath))
+
+def getOptions():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--src',  required=True,  help='source dir of the btrfs subvolume')
+    parser.add_argument('--dest', required=True,  help='destination dir to backup to. Note that in this dir a dir with SRC name is created')
+    args = parser.parse_args()
+
+    return {
+        "src":  scriptRelative(args.src),
+        "dest": scriptRelative(args.dest)
+    }
+
+options = getOptions()
+
+print("{}".format(os.path.dirname(__file__)))
+
+src       = options["src"]
+src_name  = path.basename(src)
+dest_base = options["dest"]
+dest      = path.join(dest_base, src_name)
+src_snaps = path.join(src, "/../.snapshot_"+src_name)
+
+if not path.isdir(dest):
+    print("Destination dir {} does not exist".format(dest))
+    print("should it created: y/n")
+    choose = input()
+    if choose == "y":   os.mkdir(dest)
+    else:               errorReturn("Dir {} required for backup".format(dest))
 
 for iDir in [src, dest, src_snaps]:
-    if not os.path.isdir(iDir): 
-        print("[ERROR] dir {} does not exists".format(iDir)); 
-        sys.exit(1)
+    if not os.path.isdir(iDir):
+        errorReturn("[ERROR] dir {} does not exists".format(iDir))
 
 snapshots=[]
 for idx, iSnap in enumerate(os.listdir(src_snaps)):
@@ -23,17 +56,14 @@ for idx, iSnap in enumerate(os.listdir(src_snaps)):
 
     entry = {}
     
-    snap_src  = src_snaps  + "/" + iSnap
-    snap_dest = dest       + "/" + iSnap
+    snap_src  = path.join(src_snaps, iSnap)
+    snap_dest = path.join(dest,      iSnap)
     action="none"
 
-    is_dest = os.path.isdir(snap_dest)
+    is_dest = path.isdir(snap_dest)
 
-    if is_first_snap and not is_dest:
-        action="full"
-    elif not is_dest:
-        action="incremental"
-
+    if is_first_snap and not is_dest:   action="full"
+    elif not is_dest:                   action="incremental"
 
     entry["name"]          = iSnap
     entry["src"]           = snap_src
@@ -54,11 +84,14 @@ print("SRC      = {}".format(src))
 print("DEST     = {}".format(dest))
 print("BASENAME = {}".format(src_name))
 print("SRC_SNAP = {}".format(src_snaps))
-
 print("SNAPSHOTS")
 for iSnap in snapshots:
     print("    {}: action={}; parent={}  ".format(iSnap["name"], iSnap["action"], iSnap["parent"]))
 
+
+#-------------------
+#Execute
+#-------------------
 print("Are you sure    yes/no/dry")
 choose=input()
 
@@ -75,15 +108,11 @@ for idx, iSnap in enumerate(snapshots):
   
     command=""
 
-    if is_full:
-        command="btrfs send -q {} | btrfs receive {}".format(iSnap["src"], dest)
-    elif is_incr:
-        command="btrfs send -p {} {} | btrfs receive {}".format(iSnap["parent_path"], iSnap["src"], dest)
-    else: continue
+    if is_full:     command="btrfs send -q {}    | btrfs receive {}".format(iSnap["src"], dest)
+    elif is_incr:   command="btrfs send -p {} {} | btrfs receive {}".format(iSnap["parent_path"], iSnap["src"], dest)
+    else:           continue
     
     print(command)
 
-    if is_dryrun:
-        continue
-    elif is_run:
-        os.system(command)
+    if is_dryrun:   continue
+    elif is_run:    os.system(command)
