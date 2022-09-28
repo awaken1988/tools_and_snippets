64d45ed88b15f47bc076e2e2f26cb886f13d44e1
Martin K <martin.awake1@gmail.com>
2022-09-28 22:35:41 +0200

    new folder structure

--- installscripts/3_git.py
+++
@@ -1,7 +0,0 @@
-from os import system
-
-#-------------------------------
-# git
-#-------------------------------
-system("git config --global core.filemode false")
-system("git config --global core.autocrlf false")
