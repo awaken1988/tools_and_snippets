aaf0e51b4f80a29a3e6cdc9c60967aa213986590
Martin K <martin.awake1@gmail.com>
2022-12-11 17:40:32 +0100

    tftp_server cleanpu connections after close

--- tools/rust/tftp_server/src/main.rs
+++ tools/rust/tftp_server/src/main.rs
@@ -2,21 +2,26 @@ use std::{
     net::{UdpSocket, SocketAddr}, 
     collections::HashMap, 
     sync::{mpsc::{Sender, channel}, Arc, Mutex},
-    thread};
+    thread, time::{Duration, Instant}};
 
 
 
 mod connection;
 mod mydef;
-
 use mydef::ClientState;
+
+const CLEANUP_TIMEOUT: Duration = Duration::from_secs(10);
 
 fn main() {
     let mut connections = HashMap::<SocketAddr,ClientState>::new();
     //let root = "C:/tftp".to_string();
     let root = "/home/martin/src/".to_string();
 
+    let mut cleanpup_stopwatch = Instant::now();
+
+
     let socket = UdpSocket::bind("127.0.0.1:69").unwrap();
+    socket.set_read_timeout(Some(Duration::from_secs(1)));
 
     let (ctrl_sender, ctrl_recv) = channel::<SocketAddr>();
 
@@ -24,8 +29,13 @@ fn main() {
         let mut buf = Vec::<u8>::new();
         buf.resize(4096, 0);
 
-    
-        let (amt, src) = socket.recv_from(&mut buf).unwrap();
+        let (amt, src) = match socket.recv_from(&mut buf) {
+            Ok((size,socket)) => (size,socket),
+            Error => {
+                cleanup_connections(&mut connections, &mut cleanpup_stopwatch);
+                continue;
+            }
+        };
 
         buf.resize(amt, 0);
     
@@ -61,9 +71,28 @@ fn main() {
         }
 
         //cleanup
-        
+        cleanup_connections(&mut connections, &mut cleanpup_stopwatch);
 
+    }
+}
+
+fn cleanup_connections(connections: &mut HashMap::<SocketAddr,ClientState>, stopwatch: &mut Instant) {
+    if stopwatch.elapsed() < CLEANUP_TIMEOUT {
+        return;
+    }
+
+    let mut todo_delete: Vec<SocketAddr> = vec![];
+    for i_con in connections.iter_mut() {
+        if i_con.1.join_handle.as_mut().unwrap().is_finished() {
+            todo_delete.push(*i_con.0);
+        }
+    }
+    for i_con in todo_delete {
+        let state = connections.remove(&i_con).unwrap();
+        println!("Cleanput connection={:?}", i_con);
+        state.join_handle.unwrap().join();
     }
 
 
+    *stopwatch = Instant::now();
 }
