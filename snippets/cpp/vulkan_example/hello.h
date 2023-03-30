446b253cf2f48ed19cadf8a4a598fb7e08bd4423
Martin K <martin.awake1@gmail.com>
2023-03-30 23:40:11 +0200

    physical devices

--- snippets/cpp/vulkan_example/hello.h
+++ snippets/cpp/vulkan_example/hello.h
@@ -46,33 +46,26 @@ private:
         this->window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", nullptr, nullptr);
 
         createInstance();
+        //setupDebugMessenger();
+        pickPhysicalDevice();
     }
 
     void createInstance() {
-        VkApplicationInfo appInfo{};
-        appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
-        appInfo.pApplicationName = "hello";
-        appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
-        appInfo.pEngineName = "No Engine";
-        appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
-        appInfo.apiVersion = VK_API_VERSION_1_0;
+        const std::vector<const char*> validationLayers = {"VK_LAYER_KHRONOS_validation"};
+        std::vector<const char*> extensionsUsed;
 
-        VkInstanceCreateInfo createInfo{};
-        createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
-        createInfo.pApplicationInfo = &appInfo;
+        //add glfw extension
+        uint32_t glfwExtensionCount = 0;
+        const char** glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
 
         //glfw extension
         {
-            uint32_t glfwExtensionCount = 0;
-            const char** glfwExtensions;
-             glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
-             cout << "Required Extensions" << endl;
+            cout << "Required Extensions" << endl;
             for(uint32_t iExt=0; iExt<glfwExtensionCount; iExt++) {
                 cout << "   " << glfwExtensions[iExt] << endl;
             }
 
-            createInfo.enabledExtensionCount = glfwExtensionCount;
-            createInfo.ppEnabledExtensionNames = glfwExtensions;
+            extensionsUsed.insert(extensionsUsed.end(), glfwExtensions, glfwExtensions+glfwExtensionCount);
         }
 
         //extensions
@@ -84,6 +77,14 @@ private:
             cout << "Extensions" << endl;
             for(const auto& iExt: this->extensions) {
                 cout << "   " << iExt.extensionName << endl;
+            }   
+        }
+
+        //extension used
+        {
+            cout << "Extensions Used" << endl;
+            for(const auto& iExt: extensionsUsed) {
+                cout << "   " << iExt << endl;
             }   
         }
 
@@ -101,7 +102,7 @@ private:
 
         //layers used
         {   
-            const std::vector<const char*> validationLayers = {"VK_LAYER_KHRONOS_validation..."};
+            
             bool found = false;
             for(const auto& iWanted: validationLayers) {
                 for(const auto& iLayer: this->layers) {
@@ -115,14 +116,57 @@ private:
             }
         }
 
+        VkApplicationInfo appInfo{};
+        appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+        appInfo.pApplicationName = "hello";
+        appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+        appInfo.pEngineName = "No Engine";
+        appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+        appInfo.apiVersion = VK_API_VERSION_1_0;
+
+        VkInstanceCreateInfo createInfo{};
+        createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+        createInfo.pApplicationInfo = &appInfo;
+        createInfo.enabledLayerCount = validationLayers.size();
+        createInfo.ppEnabledLayerNames = validationLayers.data();
+        createInfo.enabledExtensionCount = extensionsUsed.size();
+        createInfo.ppEnabledExtensionNames = extensionsUsed.data();
+
+
         if(vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS) {
             throw string{"vkCreateInstance failed"};
         }
     }
 
+    void pickPhysicalDevice() {
+        uint32_t deviceCount = 0;
+        std::vector<VkPhysicalDevice> physicalDevices;
+        
+        vkEnumeratePhysicalDevices(instance,&deviceCount, nullptr);
+        physicalDevices.resize(deviceCount);
+        vkEnumeratePhysicalDevices(instance, &deviceCount, physicalDevices.data());
+
+        if(physicalDevices.size() == 0) {
+            throw string{"no physicalDevices found"};
+        }
+
+        cout << "Devices Available" << endl;
+        for(const auto& iDev: physicalDevices) {
+            VkPhysicalDeviceProperties props;
+            vkGetPhysicalDeviceProperties(iDev, &props);
+            cout << "   " << props.deviceName << "; " << props.apiVersion << endl;
+        }
+    }
+
+    bool isDeviceSuitable(VkPhysicalDevice device) {
+        return true;
+    }
+
 private:
     GLFWwindow* window;
     VkInstance  instance;
+    VkPhysicalDevice physicalDevice = VK_NULL_HANDLE;
+
     std::vector<VkExtensionProperties> extensions;
     std::vector<VkLayerProperties> layers;
 };
