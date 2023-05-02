c22584bf18cc6a9f46c4dae09332da1393e6c9c1
Martin K <martin.awake1@gmail.com>
2023-05-02 21:14:51 +0200

    begin vertex

--- snippets/cpp/vulkan_example/hello.h
+++ snippets/cpp/vulkan_example/hello.h
@@ -3,6 +3,8 @@
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
 
+#include <glm/glm.hpp>
+
 #include <vulkan/vulkan.h>
 #include <iostream>
 #include <stdexcept>
@@ -15,6 +17,7 @@
 #include <limits>
 #include <algorithm>
 #include <fstream>
+#include <array>
 
 using std::cout;
 using std::endl;
@@ -35,6 +38,36 @@ static std::vector<char> readFile(const std::string& filename) {
 
     return buffer;
 }
+
+struct Vertex {
+    glm::vec2 pos;
+    glm::vec3 color;
+
+    static VkVertexInputBindingDescription getBindingDescription() {
+        VkVertexInputBindingDescription bindingDescription{};
+        bindingDescription.binding = 0;
+        bindingDescription.stride = sizeof(Vertex);
+        bindingDescription.inputRate = VK_VERTEX_INPUT_RATE_VERTEX;
+        return bindingDescription;
+    }
+
+    static std::array<VkVertexInputAttributeDescription, 2> getAttributeDescriptions() {
+        std::array<VkVertexInputAttributeDescription, 2> attributeDescriptions{};
+
+        attributeDescriptions[0].binding = 0;
+        attributeDescriptions[0].location = 0;
+        attributeDescriptions[0].format = VK_FORMAT_R32G32_SFLOAT;
+        attributeDescriptions[0].offset = offsetof(Vertex, pos);
+
+        attributeDescriptions[1].binding = 0;
+        attributeDescriptions[1].location = 1;
+        attributeDescriptions[1].format = VK_FORMAT_R32G32B32_SFLOAT;
+        attributeDescriptions[1].offset = offsetof(Vertex, color);
+
+
+        return attributeDescriptions;
+    }
+};
 
 
 class Hello {
@@ -63,6 +96,7 @@ public:
 
 
     void run() {
+        initVertices();
         initVulkan();
         mainLoop();
         cleanup();
@@ -564,12 +598,16 @@ private:
         dynamicState.pDynamicStates = dynamicStates.data();
         
         //vertex input
+        auto bindingDescriptions = Vertex::getBindingDescription();
+        auto attributeDescriptions = Vertex::getAttributeDescriptions();
+        
         VkPipelineVertexInputStateCreateInfo vertexInputInfo{};
         vertexInputInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_VERTEX_INPUT_STATE_CREATE_INFO;
-        vertexInputInfo.vertexBindingDescriptionCount = 0;
-        vertexInputInfo.pVertexBindingDescriptions = nullptr; // Optional
-        vertexInputInfo.vertexAttributeDescriptionCount = 0;
-        vertexInputInfo.pVertexAttributeDescriptions = nullptr; // Optional
+        vertexInputInfo.vertexBindingDescriptionCount = 1;
+        vertexInputInfo.pVertexBindingDescriptions = &bindingDescriptions; // Optional
+        vertexInputInfo.vertexAttributeDescriptionCount = static_cast<uint32_t>(attributeDescriptions.size());
+        vertexInputInfo.pVertexAttributeDescriptions = attributeDescriptions.data(); // Optional
+     
 
         VkPipelineInputAssemblyStateCreateInfo inputAssembly{};
         inputAssembly.sType = VK_STRUCTURE_TYPE_PIPELINE_INPUT_ASSEMBLY_STATE_CREATE_INFO;
@@ -895,7 +933,14 @@ private:
             {
                 throw string("failed to create semaphores!");
             }
+    }
 
+    void initVertices() {
+        m_vertices = {
+            {{0.0f, -0.5f}, {1.0f, 0.0f, 0.0f}},
+            {{0.5f, 0.5f}, {0.0f, 1.0f, 0.0f}},
+            {{-0.5f, 0.5f}, {0.0f, 0.0f, 1.0f}}
+        };
     }
     
 private:
@@ -927,6 +972,8 @@ private:
 
     std::vector<VkImage> m_swapChainImages;
     std::vector<VkImageView> m_swapChainImageViews;
+
+    std::vector<Vertex> m_vertices;
 
     uint64_t m_drawCalls = 0;
 };
